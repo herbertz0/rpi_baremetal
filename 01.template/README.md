932889b07dd3a8e63e44c410669a7356d8d21652
Alan Gardin <alan.gardin@protonmail.com>
2016-07-09 13:12:30 +0200

    Tweaked original files and adapted Makefile

--- 01.template/README.md
+++ 01.template/README.md
@@ -1,2 +1,7 @@
 #01
 Taken from: https://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/os/ok01.html
+
+Questions
+---------
+- [ ] Linker script: code start at 0x800. Reference?
+- [ ] Discard section?
--- 01.template/src/main.s
+++ 01.template/src/main.s
@@ -4,17 +4,18 @@
 ******************************************************************************/
 
 /*
-* .section is a directive to our assembler telling it to place this code first.
-* .globl is a directive to our assembler, that tells it to export this symbol
-* to the elf file. Convention dictates that the symbol _start is used for the 
+* Convention dictates that the symbol _start is used for the 
 * entry point, so this all has the net effect of setting the entry point here.
 * Ultimately, this is useless as the elf itself is not used in the final 
 * result, and so the entry point really doesn't matter, but it aids clarity,
 * allows simulators to run the elf, and also stops us getting a linker warning
 * about having no entry point. 
 */
+
+
 .section .init
+
 .globl _start
 _start:
+	add r0, r1
 
-// Code goes here
