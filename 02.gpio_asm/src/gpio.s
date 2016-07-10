932303136a0a9cab2ec9a08e786ec2ccb67dd022
Alan Gardin <alan.gardin@protonmail.com>
2016-07-10 18:21:42 +0200

    Tested brute force method (overwriting any gpio pins)

--- 02.gpio_asm/src/gpio.s
+++ 02.gpio_asm/src/gpio.s
@@ -22,32 +22,41 @@
 // Sets the pin as output
 gpio_init_statusled:
 	ldr r0,=GPFSEL1
+	/*
 	ldr r1, [r0]
 	mov r2, #0b111
 	lsl r2, #FSEL16
 	bic r1, r1, r2
 	mov r2, #1
 	lsl r2, #FSEL16
-	orr r1, r1, r2
+	orr r1, r1, r2*/
+	mov r1, #1
+	lsl r1, #FSEL16
 	str r1, [r1]
 	mov pc, lr
 
 // Outputs logical 1
 gpio_set_statusled:
 	ldr r0, =GPSET0
+	/*
 	ldr r1, [r0]
 	mov r2, #1
 	lsl r2, #16
-	orr r1, r1, r2
+	orr r1, r1, r2*/
+	mov r1, #1
+	lsl r1, #16
 	str r1, [r0]
 	mov pc, lr
 
 // Outputs logical 0
 gpio_clear_statusled:
 	ldr r0, =GPCLR0
+	/*
 	ldr r1, [r0]
 	mov r2, #1
 	lsl r2, #16
-	orr r1, r1, r2
+	orr r1, r1, r2*/
+	mov r1, #1
+	lsl r1, #16
 	str r1, [r0]
 	mov pc, lr
--- 02.gpio_asm/src/main.s
+++ 02.gpio_asm/src/main.s
@@ -9,5 +9,6 @@
 _start:
 	bl gpio_init_statusled
 	bl gpio_clear_statusled
+
 halt:
 	b halt
